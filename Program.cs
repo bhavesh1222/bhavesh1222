6e7d7b1d32f5558cc4f7a572fd7739fb246c2fc1
bhavesh <bdbhadane200@gmail.com>
2022-10-11 11:46:11 +0530

    add program.cs file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace checkusername
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("helloworld");
+            string name = Console.ReadLine();
+            int count = name.Length;
+            Console.WriteLine("the length is " + count);
+            if (name.Length >= 3)
+            {
+                Console.WriteLine("helloworldfile" + name + "how are you?");
+            }
+        }
+
+    }
+
+
+
+}
+
